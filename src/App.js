6866032194ffb536dd4d8752403fa409a2a9237a
mishraom12345 <mishrahariom525@gmail.com>
2023-02-15 10:04:31 +0530

    fetching data using fetch and async await

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
+
 function App() {
+  const [movie ,setmovie] =useState([])
+  async function fectchhandler(){
+  const response=  await fetch('https://swapi.py4e.com/api/films/')
+  const data = await response.json()
+
+      const maindata = data.results.map(item=>{
+        return{
+          id:item.episode_id,
+          title:item.title,
+          openingText:item.opening_crawl,
+          releaseDate:item.release_date
+
+        }
+      })
+      setmovie(maindata)
+    }
+  
   
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fectchhandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movie} />
       </section>
     </React.Fragment>
   );
